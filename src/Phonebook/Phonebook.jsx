56bcb713bc1de7e659540123ff7a0c8f26839ae6
Juliya Khanenko <jullietnm@gmail.com>
2022-10-17 22:09:32 +0300

    ???

--- src/Phonebook/Phonebook.jsx
+++ src/Phonebook/Phonebook.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 
 class PhoneBook extends Component {
   rende() {
     return (
       <div>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        ;
+        <h2>Phonebook</h2>
+        <div>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          ;
+        </div>
+        <h3>Contacts</h3>
+        <p> Find contacts by name</p>
       </div>
     );
   }
