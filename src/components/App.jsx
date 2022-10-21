7c1e9c676f20278462ac3f43653c3210fc43f2f0
Juliya Khanenko <jullietnm@gmail.com>
2022-10-22 00:01:59 +0300

    alert

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,11 +14,16 @@ class App extends Component {
   };
 
   addContact = (name, number) => {
+    const { contacts } = this.state;
     const newContact = {
       id: nanoid(),
       name,
       number,
     };
+    if (contacts.find(contact => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
 
     this.setState(prevState => ({
       contacts: [newContact, ...prevState.contacts],
